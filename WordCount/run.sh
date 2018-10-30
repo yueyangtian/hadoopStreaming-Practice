ddf9f9946ef0ce61c290c1aaba7d306df2a892b5
yueyangtian <pawnsir@163.com>
2018-10-30 18:23:29 +0800

    change run shell for hadoop 2.0

--- WordCount/run.sh
+++ WordCount/run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-STREAM_JAR_PATH="/root/hadoop-1.2.1/contrib/streaming/hadoop-streaming-1.2.1.jar"
+STREAM_JAR_PATH="/root/hadoop-2.8.4/share/hadoop/tools/lib/hadoop-streaming-2.8.4.jar"
 INPUT_FILE_PATH="/123.txt"
 OUTPUT_PATH="/output"
 
-/root/hadoop-1.2.1/bin/hadoop fs -rmr -skipTrash $OUTPUT_PATH
-/root/hadoop-1.2.1/bin/hadoop jar $STREAM_JAR_PATH \
+/root/hadoop-2.8.4/bin/hadoop fs -rmr -skipTrash $OUTPUT_PATH
+/root/hadoop-2.8.4/bin/hadoop jar $STREAM_JAR_PATH \
         -input $INPUT_FILE_PATH \
         -output $OUTPUT_PATH \
         -jobconf "mapred.job.name=test" \
